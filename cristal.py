a1b002b6955a4c9f10a7cd1174a8b917bee1a0ba
Andrey <doto1816@gmail.com>
2024-05-06 11:23:38 -0500

    Se agrega el documento que tiene un conjunto de funciones de las propiedades de los materiales cristalinos, primero con la funcion para encontrar el volumen de una celda cubica

---
+++ cristal.py
@@ -0,0 +1,15 @@
+#Cristalino 
+
+def v_cubic(a): 
+	'''
+        (num) -> float
+	
+	Retorna el volumen de una estructura cubica 
+	con parametro de red a.
+
+	>>> v_cubic(2)
+	8.0
+	>>> v_cubic(3)
+	27.0
+        '''
+	return a**3
