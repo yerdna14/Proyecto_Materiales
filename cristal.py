dea8d6c3eaf43634970e12bf6657b2ddd703dc4e
Andrey <doto1816@gmail.com>
2024-05-16 08:13:56 -0500

    Una nueva funcion para calcular el volumen de una estructura tetahedrica

--- cristal.py
+++ cristal.py
@@ -13,3 +13,17 @@ def v_cubic(a):
 	27.0
         '''
 	return a**3
+<<<<<<< HEAD
+
+def v_tet(a,b):
+	'''(number, number) -> number
+	Retorna el volumen de un solido con estructura 
+	tetaedrica con parametros de red a,b.
+	>>> v_tet(2,3)
+	12
+	>>> v_tet(3,2)
+	18
+	'''
+	return a**2*b 
+=======
+>>>>>>> a1b002b (Se agrega el documento que tiene un conjunto de funciones de las propiedades de los materiales cristalinos, primero con la funcion para encontrar el volumen de una celda cubica)
