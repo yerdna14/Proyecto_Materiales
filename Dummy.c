bbeffec2b280fb60426085f22dd80a2314e9c6be
Andrey <doto1816@gmail.com>
2024-06-12 12:12:34 -0500

    En este caso actualizamos el contexto del repo.

--- Dummy.c
+++ Dummy.c
@@ -1,4 +1,7 @@
 Hello World,
 Parabens Andrey muito bom trabalho
 
-Parabens Bro
+Parabens Bro.
+
+Ahora vamos a empezar haciendo un borrador de las primeras funciones 
+para el diseño de materiales
