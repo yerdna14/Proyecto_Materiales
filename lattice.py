218dc3750bfa06ba01107308810b7e0b68a422fd
Andrey <doto1816@gmail.com>
2025-04-23 13:54:53 -0500

    comentar el autor

--- lattice.py
+++ lattice.py
@@ -1,5 +1,5 @@
 #Pronto mas en este 2025
-@Author:Andrey
+# @Author:Andrey
 import math
 #Paquete de funciones para calcular el parametro de red a0  de diferentes estructuras cristalinas
 def lat_sc(r):
